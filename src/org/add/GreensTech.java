439d306d237ffc72bc371d31f4cf7b9d6beffd01
pskselvakumarsk <pskselvakumar.sk@gmail.com>
2022-02-23 15:32:27 +0530

    Greens Project

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,22 @@
+package org.add;
+
+public class GreensTech {
+	
+	public void greensOmr() {
+		System.out.println("GOMR");
+	}
+	public void greensAdayar() {
+		System.out.println("GADR");
+	}
+	public void greensAnnaNagar() {
+		System.out.println("GANNANGR");
+	}
+	
+	public static void main(String[] args) {
+		GreensTech gt = new GreensTech();
+		gt.greensOmr();
+		gt.greensAdayar();
+		gt.greensAnnaNagar();
+	}
+
+}
